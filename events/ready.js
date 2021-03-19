1e8b5cf08b8a81d4e643657ab719c3df3ae7e0de
LunaOk <70438097+XLunaSoft@users.noreply.github.com>
2021-03-18 23:44:02 -0600

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,5 @@
+module.exports = async (client) => {
+  console.log(`...  Conectando - ${client.user.username}`);
+	  console.log(`...Conectado Con - ${client.user.username} ✔`);
+			  console.log(`... `);
+				console.log(`--BIENVENIDO!--`);};
